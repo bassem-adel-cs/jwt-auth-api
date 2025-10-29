c12885a7a614afa8408ff6320e3f164a7701fd62
Bassem adel <139783226+bassem-adel-cs@users.noreply.github.com>
2025-10-29 17:21:46 +0300

    Update loginRoute.js

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -6,5 +6,5 @@ const control=require("../controller/logincontroller")
 const registcontroller=require("../controller/registcontroller")
  route.get("/users",control.getallusers);
  route.post("/login",loginvalidation,control.checkloginusers)
- route.post("/regist",registvalidation,registcontroller)
+ route.post("/register",registvalidation,registcontroller)
  module.exports=route
